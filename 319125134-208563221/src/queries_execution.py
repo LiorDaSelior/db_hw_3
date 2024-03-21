61dafe9e0506e98773f24680649920b489ad08b2
LiorDaSelior <liorbelenkov@mail.tau.ac.il>
2024-03-21 21:25:49 +0200

    added more query examples

--- 319125134-208563221/src/queries_execution.py
+++ 319125134-208563221/src/queries_execution.py
@@ -2,11 +2,23 @@ from queries_db_script import *
 
 if __name__ == "__main__":
     query_1("Director", "Action")
+    
     print("\n***\n")
+    
     query_2(2015)
+    
     print("\n***\n")
+    
     query_3("Actor", 2018)
+    
     print("\n***\n")
+    
     query_4("johnson")
+    
     print("\n***\n")
-    query_5("mafia", "mafia", "mafia")
+    
+    query_5("war", "world", "battle") 
+    
+    print("\n***\n")
+    
+    query_5("love", "fire", "fire") # example of search with 1 or 2 keywords instead - just repeat a used keyword in parameters
